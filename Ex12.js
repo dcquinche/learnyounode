d49fc1746e532fdec626f2658014aa576e99af0d
GDiana <dcquinche@gmail.com>
2022-10-24 19:17:28 -0500

    Solution exercise 12

---
+++ Ex12.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const map = require('through2-map');
+const server = http.createServer(callback);
+const port = process.argv[2];
+
+function callback(req, res){
+    if(req.method === 'POST'){
+        req.pipe(map(function (chunk) {
+            return chunk.toString().toUpperCase()
+          })).pipe(res)
+    }
+}
+
+server.listen(Number(port));
